6ffdbc58ea8eb715c20b28edbbca76c3da0e78e6
CedricDoerr <doerr.cedric@gmail.com>
2022-01-30 20:56:17 +0100

    RSA_Key

--- CSS/rsa_key.py
+++ CSS/rsa_key.py
@@ -18,5 +18,7 @@ def euklid(a_0,b_0):
         i += 1
     return a,b,q_ek,r
 
+""" n =p*q phi = (p-1)(q-1) e = 65537 d = 65537^-1 mod phi """
+
 print(euklid(240120,65537))
 print("P = "+str(p)+" Q = "+str(q)+" N = "+str(n)+" Phi = "+str(phi)+" E = "+str(e)+" D = "+str(d))
